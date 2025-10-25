c1cb106ac8f98f960444880b691949900a756b84
Eduardo <eduardo@example.com>
2025-10-25 15:34:17 -0300

    Change: Set login page as homepage instead of Laravel welcome page

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\Auth\GoogleController;
 use App\Http\Controllers\Auth\LoginController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Route::middleware('guest')->group(function () {

42a72ad869d851ddcb126234039493a09cd80c36
Eduardo <eduardo@example.com>
2025-10-30 23:04:41 -0300

    Fix user dropdown menu in navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -71,12 +71,12 @@
             <ul class="navbar-nav ml-auto">
                 <!-- User Dropdown Menu -->
                 <li class="nav-item dropdown">
-                    <a class="nav-link" data-toggle="dropdown" href="#">
+                    <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">
                         <i class="fas fa-user"></i>
                         <span class="d-none d-md-inline">{{ auth()->user()->name }}</span>
                         <i class="fas fa-angle-down ml-1"></i>
                     </a>
-                    <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+                    <div class="dropdown-menu dropdown-menu-lg dropdown-menu-end">
                         <div class="dropdown-divider"></div>
                         <a href="{{ route('dashboard') }}" class="dropdown-item">
                             <i class="fas fa-user-edit mr-2"></i> Mi Perfil
